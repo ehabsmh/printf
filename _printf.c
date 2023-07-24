24e7da58666bc3b125629066fa4ec5691bb860c6
MaiElNaggar <eng.maimohamed@gmail.com>
2023-07-24 15:14:38 -0700

    add _printf.c file

---
+++ _printf.c
@@ -0,0 +1,22 @@
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input char.
+ *
+ * Return: number of characters printed.
+ */
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+  int output_len = 0;
+  va_list args;
+
+  va_start(args, format);
+  output_len = format_handler(&args, format);
+  /* flags */
+  /* width */
+  /* precision */
+  va_end(args);
+
+  return (output_len);
+}
