3c69a90d2171bdc066ab5f8212e450093e60a38c
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-25 16:30:09 +0300

    format_handler.c - format specifiers of  handled

---
+++ format_handler.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * format_handler - handling the format
+ * @args: va_list
+ * @format: the string at the first argument
+ *
+ * Return: length of the output
+*/
+int format_handler(va_list *args, const char *format)
+{
+	int i, j, output_len = 0, buffer_index = 0;
+	int (*ptr)(va_list *, int *, char *);
+	char buffer[BUFFER_SIZE];
+
+	fmt format_sp[] = {
+			{'c', print_char}, {'s', print_str}, {'%', print_percent},
+			{'\0', NULL}};
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			for (j = 0; format_sp[j].specifier != '\0'; j++)
+			{
+				if (format[i] == format_sp[j].specifier)
+				{
+					ptr = format_sp[j].print_format;
+					output_len += (*ptr)(args, &buffer_index, buffer);
+					break;
+				}
+			}
+		}
+		else
+		{
+			if (buffer_index < BUFFER_SIZE - 1)
+			{
+				buffer[buffer_index] = format[i];
+				buffer_index++;
+			}
+			output_len++;
+		}
+	}
+	buffer[buffer_index] = '\0';
+	write(1, buffer, buffer_index);
+	return (output_len);
+}
